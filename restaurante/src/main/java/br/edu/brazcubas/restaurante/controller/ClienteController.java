59e63972f6aa8a4968b57a3066d203e21145a12c
rafael <calixxtorafell@gmail.com>
2024-05-19 11:47:17 -0300

    update: ClienteController

--- restaurante/src/main/java/br/edu/brazcubas/restaurante/controller/ClienteController.java
+++ restaurante/src/main/java/br/edu/brazcubas/restaurante/controller/ClienteController.java
@@ -2,6 +2,9 @@ package br.edu.brazcubas.restaurante.controller;
 
 import br.edu.brazcubas.restaurante.model.entity.Cliente;
 
+import java.sql.SQLException;
+import java.util.List;
+
 import br.edu.brazcubas.restaurante.model.dao.IDAO;
 
 public class ClienteController {
@@ -11,8 +14,26 @@ public class ClienteController {
         this.dao = dao;
     }
 
-    public String cadastrarCliente(Cliente cliente){
+    public String cadastrarCliente(Cliente cliente) throws SQLException{
         dao.registrar(cliente);
         return "Cadastrado com sucesso!";
+    }
+
+    public String atualizar(Cliente cliente) throws SQLException {
+        dao.atualizar(cliente);
+        return "Atualizado com sucesso!";
+    }
+
+    public String excluir(int id) throws SQLException {
+        dao.excluir(id);
+        return "Excluido com sucesso!";
+    }
+
+    public List<Cliente> retornarTodos(){
+        return dao.retornarTodos();
+    }
+
+    public Cliente retornarPorId(int id){
+        return dao.retornarPorId(id);
     }
 }
